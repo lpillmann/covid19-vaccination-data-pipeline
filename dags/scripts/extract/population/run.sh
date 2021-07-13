a5a7326c4193a57fae4d89c6f7281a69e7e917b7
Lui Pillmann <lui.pillmann@gmail.com>
2021-07-13 18:23:49 -0300

    Update extract population script

--- dags/scripts/extract/population/run.sh
+++ dags/scripts/extract/population/run.sh
@@ -2,13 +2,17 @@
 
 # Load population CSV file to S3
 # Source: IBGE, data treated and shared by Álvaro Justen/Brasil.IO
+# Usage: 
+#    - Extract and append to destination:     `bash run.sh`
+#    - Extract and replace existing contents:  `bash run.sh replace`
 
 set -e
+
+LOAD_MODE=$1
 
 # Set credentials
 AWS_KEY="$UDACITY_AWS_KEY"
 AWS_SECRET="$UDACITY_AWS_SECRET"
-AWS_PROFILE="$UDACITY_AWS_PROFILE"
 
 # Configure S3 path
 S3_BUCKET=$UDACITY_CAPSTONE_PROJECT_BUCKET
@@ -23,8 +27,16 @@ OUTPUT_FILENAME="population-by-city-2020.csv"
 tmp_filepath="/tmp/$OUTPUT_FILENAME"
 wget --output-document "$tmp_filepath" "$URL"
 
+if [ "$LOAD_MODE" = "replace" ]
+then
+    # Clean up S3 destination
+    remove_from_destination="$s3_uri"
+    echo "Replace mode: Removing current file at $remove_from_destination"
+    /opt/airflow/venvs/awscli/bin/aws s3 rm "$remove_from_destination" --include "*.csv*" --recursive
+fi
+
 # Load into S3
-aws s3 cp "$tmp_filepath" "$s3_uri" --profile "$AWS_PROFILE"
+/opt/airflow/venvs/awscli/bin/aws s3 cp "$tmp_filepath" "$s3_uri"
 
 # Remove tmp file
 rm $tmp_filepath
